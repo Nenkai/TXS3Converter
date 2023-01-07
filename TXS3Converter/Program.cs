ece12d4f699950b6bea2a7f0c29e888eba41f31a
Nenkai <Nenkai@users.noreply.github.com>
2023-01-07 20:02:29 +0100

    Support for GT7 #2

--- TXS3Converter/Program.cs
+++ TXS3Converter/Program.cs
@@ -2,113 +2,99 @@
 using System.Linq;
 using System.IO;
 using System.Text;
+
+using PDTools.Files.Textures;
+using PDTools.Files.Models;
+
+using Syroot.BinaryData;
 using System.Collections.Generic;
 
-using GTTools.Formats;
-
-using CommandLine;
 using CommandLine.Text;
+using CommandLine;
+using static PDTools.Files.Textures.TextureSet3;
 
 namespace GTTools
 {
     class Program
     {
+        public static bool isBatchConvert = false;
+        private static int processedFiles = 0;
         public static string currentFileName;
-        public static bool _texConvChecked = false;
-        public static bool _texConvExists = false;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Gran Turismo TXS3 Converter");
-            if (args.Length == 1)
-            {
-                if (Directory.Exists(args[0]))
-                {
-                    foreach (var file in Directory.GetFiles(args[0], ".img"))
-                    {
-                        ProcessFile(file);
-                        if (!_texConvExists)
-                            return;
-                    }
+            Console.WriteLine("Gran Turismo Texture Set (TXS3) / PDI Texture (PDI0) Converter");
 
-                    Console.WriteLine($"Done.");
-                    return;
-                }
-                else if (File.Exists(args[0])) 
-                {
-                    ProcessFile(args[0]);
-                    Console.WriteLine($"Done.");
-                    return;
-                }
-            }
-
-            Parser.Default.ParseArguments<TexConvImgOptions, TexConvTXSOptions>(args)
-                .WithParsed<TexConvImgOptions>(TexConvImg)
-                .WithParsed<TexConvTXSOptions>(TexConvTXS);
+            var p = Parser.Default.ParseArguments<ConvertToPngVerbs, ConvertToImgVerbs>(args);
+            p.WithParsed<ConvertToPngVerbs>(ConvertToPng)
+             .WithParsed<ConvertToImgVerbs>(ConvertToImg)
+             .WithNotParsed(e => { });
         }
 
-        public static void TexConvImg(TexConvImgOptions option)
+        public static void ConvertToPng(ConvertToPngVerbs verbs)
         {
-            TXS3ImageFormat format = option.Format switch
+            foreach (var file in verbs.InputPath)
             {
-                "DXT1" => TXS3ImageFormat.DXT1,
-                "DXT3" => TXS3ImageFormat.DXT3,
-                "DXT5" => TXS3ImageFormat.DXT5,
-                "DXT10" => TXS3ImageFormat.DXT10,
-            };
-
-            if (format == TXS3ImageFormat.Unknown)
-            {
-                Console.WriteLine("Wrong format. Expected DXT1/DXT3/DXT5/DXT10.");
-                return;
-            }
-
-
-            foreach (var file in option.InputFiles)
-            {
-                if (!File.Exists(file))
+                if (!File.Exists(file) && !Directory.Exists(file))
                 {
-                    Console.WriteLine($"File '{file}' does not exist.");
+                    Console.WriteLine($"File does not exist: {file}");
                     continue;
                 }
 
-
-                TextureSet3 texSet = new TextureSet3();
-                texSet.WriteNames = option.WriteNames;
-                texSet.LittleEndian = option.LittleEndian;
-
-                if (texSet.AddFromStandardImage(file, format))
+                FileAttributes attr = File.GetAttributes(file);
+                if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
                 {
-                    texSet.ConvertToTXS(Path.ChangeExtension(file, ".img"));
-                    Console.WriteLine($"Converted {file} to TXS3.");
-                }
-                else
-                    Console.WriteLine($"Could not process {file}.");
-
-            }
-        }
-
-        public static void TexConvTXS(TexConvTXSOptions option)
-        {
-            foreach (var inputFile in option.InputFiles)
-            {
-                if (Directory.Exists(inputFile))
-                {
-                    foreach (var file in Directory.GetFiles(inputFile, ".img"))
+                    string[] files = Directory.GetFiles(file, "*.*", SearchOption.AllDirectories);
+                    foreach (string f in files)
                     {
-                        ProcessFile(file);
-                        if (!_texConvExists)
-                            return;
+                        currentFileName = Path.GetFileName(f);
+                        try
+                        {
+                            ConvertFileToPng(f, verbs.Format);
+                            processedFiles++;
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($@"[!] Could not convert {currentFileName} : {e.Message}");
+                        }
                     }
                 }
-                else if (File.Exists(inputFile))
+                else
                 {
-                    ProcessFile(inputFile);
+                    currentFileName = Path.GetFileName(file);
+                    try
+                    {
+                        ConvertFileToPng(file, verbs.Format);
+                        processedFiles++;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($@"[!] Could not convert {currentFileName} : {e.Message}");
+                    }
                 }
             }
+
+            Console.WriteLine($"Done, {processedFiles} files were converted.");
         }
 
-        static void ProcessFile(string path)
+        public static void ConvertToImg(ConvertToImgVerbs verbs)
+        {
+            TextureSet3 textureSet = new TextureSet3();
+
+            foreach (var file in verbs.InputPath)
+            {
+                if (AddFileToTextureSet(textureSet, file, verbs))
+                {
+                    Console.WriteLine($"Failed to add file '{file}', aborting.");
+                    return;
+                }
+            }
+
+            textureSet.ConvertToTXS("test.img");
+        }
+
+        static bool _texConvExists = false;
+        static void ConvertFileToPng(string path, TextureConsoleType consoleType)
         {
             currentFileName = Path.GetFileName(path);
             
@@ -117,48 +103,100 @@ namespace GTTools
             {
                 case "TXS3":
                 case "3SXT":
-                    ConvertTXS3Texture(path);
-                    break;
+                    ProcessTextureSetFile(path, consoleType);
+                    return;
+                case "IDP0":
+                    ProcessPDITexture(path);
+                    return;
+                case "MDL3":
+                    ProcessModelSetFile(path);
+                    return;
             }
         }
 
-        static void ConvertTXS3Texture(string path)
+        public static bool AddFileToTextureSet(TextureSet3 textureSet, string path, ConvertToImgVerbs verbs)
         {
-            if (!_texConvChecked && !File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "texconv.exe")))
+            if (verbs.Format != TextureConsoleType.PS3)
             {
-                Console.WriteLine("TexConv (image to DDS tool) is missing. Download it from https://github.com/microsoft/DirectXTex/releases and place it next to the tool.");
-                return;
+                Console.WriteLine("Currently only PS3 TXS3 can be created.");
+                return false;
             }
 
-            _texConvChecked = true;
-            _texConvExists = true;
-
-            var texSet = new TextureSet3();
-            texSet.FromFile(path);
-
-            Console.WriteLine($"Found: {texSet.Textures.Count} texture(s)");
-
-            string dir = Path.GetDirectoryName(path);
-
-            for (int i = 0; i < texSet.Textures.Count; i++)
+            // Convert if valid extension
+            string ext = Path.GetExtension(path);
+            if (ext.Equals(".png", StringComparison.OrdinalIgnoreCase) ||
+                ext.Equals(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                ext.Equals(".bmp", StringComparison.OrdinalIgnoreCase))
             {
-                var texture = texSet.Textures[i];
-                Console.WriteLine($"Format: {texture.Format} - {texture.Width}x{texture.Height} ({texture.Mipmap} mipmaps)");
-
-                if (string.IsNullOrEmpty(texture.Name))
+                if (!_texConvExists && !File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "texconv.exe")))
                 {
-                    if (texSet.Textures.Count == 1)
-                        texture.Name = Path.GetFileNameWithoutExtension(path);
-                    else
-                        texture.Name = Path.GetFileNameWithoutExtension(path) + $"_{i}";
+                    Console.WriteLine("TexConv (image to DDS tool) is missing. Download it from https://github.com/microsoft/DirectXTex/releases and place it next to the tool.");
+                    Environment.Exit(0);
                 }
-                else
-                    Console.WriteLine($"Texture Name: {texture.Name}");
+                _texConvExists = true;
 
-                texture.SaveAsPng(dir);
+                CELL_GCM_TEXTURE_FORMAT format = 0;
+                if (verbs.CellFormat.Equals("DXT1"))
+                    format = CELL_GCM_TEXTURE_FORMAT.CELL_GCM_TEXTURE_COMPRESSED_DXT1;
+                else if (verbs.CellFormat.Equals("DXT3"))
+                    format = CELL_GCM_TEXTURE_FORMAT.CELL_GCM_TEXTURE_COMPRESSED_DXT23;
+                else if (verbs.CellFormat.Equals("DXT5"))
+                    format = CELL_GCM_TEXTURE_FORMAT.CELL_GCM_TEXTURE_COMPRESSED_DXT45;
+                else if (verbs.CellFormat.Equals("DXT10"))
+                    format = CELL_GCM_TEXTURE_FORMAT.CELL_GCM_TEXTURE_A8R8G8B8;
+                else
+                {
+                    Console.WriteLine("DXT format is invalid or not provided. must be DXT1/DXT3/DXT5/DXT10.");
+                    return false;
+                }
+
+                var texture = new CellTexture();
+                /*
+                if (verbs.Swizzle)
+                    (texture.TextureRenderInfo as PGLUCellTextureInfo).FormatBits &= ~CELL_GCM_TEXTURE_FORMAT.CELL_GCM_TEXTURE_LN; // Remove the default linear flag, set up swizzle
+                */
+
+                var added = texture.FromStandardImage(path, format);
+                if (!added)
+                    return false;
+
+                textureSet.AddTexture(texture);
+
+
+                return true;
             }
+            else
+            {
+                Console.WriteLine($"Skipped {path}, no operation to be done");
+                return true;
+            }
+        }
+
+        static void ProcessTextureSetFile(string path, TextureConsoleType consoleType)
+        {
+            var txs = new TextureSet3();
+            txs.FromFile(path, consoleType);
+            txs.ConvertToPng(path);
 
             Console.WriteLine($"Converted {currentFileName} to png.");
+        }
+
+        static void ProcessPDITexture(string path)
+        {
+            var pdiTexture = new PDITexture();
+            pdiTexture.FromFile(path);
+            pdiTexture.TextureSet.ConvertToPng(path);
+
+            Console.WriteLine($"Converted {currentFileName} to png.");
+        }
+
+        static void ProcessModelSetFile(string path)
+        {
+            using var fs = new FileStream(path, FileMode.Open);
+            using var bs = new BinaryStream(fs);
+            MDL3 set = MDL3.FromStream(bs);
+
+            set.TextureSet.ConvertToPng(path);
         }
 
         static string GetFileMagic(string path)
@@ -169,36 +207,28 @@ namespace GTTools
             fs.Read(mBuf);
             return Encoding.ASCII.GetString(mBuf);
         }
+    }
 
-        static void ProcessMDL3Model(string path)
-        {
-            MDL3.FromFile(path);
-        }
+    [Verb("convert-png", HelpText = "Converts any img to png.")]
+    public class ConvertToPngVerbs
+    {
+        [Option('i', "input", Required = true, HelpText = "Input file texture or folder.")]
+        public IEnumerable<string> InputPath { get; set; }
 
-        [Verb("convertimg", HelpText = "Converts any image to TXS3.")]
-        public class TexConvImgOptions
-        {
-            [Option('i', "input", Required = true, HelpText = "Input standard images.")]
-            public IEnumerable<string> InputFiles { get; set; }
+        [Option('f', "format", HelpText = "TXS format. Currently supported is PS3 & PS4. Defaults to PS3.")]
+        public TextureConsoleType Format { get; set; } = TextureConsoleType.PS3;
+    }
 
-            [Option('f', "format", Required = true, HelpText = "Format to convert to. Can be DXT1/3/5/10.")]
-            public string Format { get; set; }
+    [Verb("convert-img", HelpText = "Converts any image to .img.")]
+    public class ConvertToImgVerbs
+    {
+        [Option('i', "input", Required = true, HelpText = "Input files for texture set")]
+        public IEnumerable<string> InputPath { get; set; }
 
-            [Option('b', "bundle", HelpText = "Creates a texset/bundle from multiple files (Unimplemented).")]
-            public bool Bundle { get; set; }
+        [Option('f', "format", Required = true, HelpText = "TXS format. Currently supported is PS3. Valid options: PS3/PS4")]
+        public TextureConsoleType Format { get; set; }
 
-            [Option('n', "name", HelpText = "Write texture names to the TXS3.")]
-            public bool WriteNames { get; set; }
-
-            [Option("le", HelpText = "Write textures as little endian. For GTPSP.")]
-            public bool LittleEndian { get; set; }
-        }
-
-        [Verb("converttxs", HelpText = "Converts any TXS3 image to standard image.")]
-        public class TexConvTXSOptions
-        {
-            [Option('i', "input", Required = true, HelpText = "Input TXS3 files.")]
-            public IEnumerable<string> InputFiles { get; set; }
-        }
+        [Option("pf", HelpText = "Pixel format when converting for PS3 - Valid options: DXT1/DXT3/DXT5/DXT10 ")]
+        public string CellFormat { get; set; }
     }
 }
